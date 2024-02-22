9ab6f4552ff573868afd4af1b3f92d3808e647c1
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-02-22 08:19:50 +0700

    Delete gwp.sh

--- gwp.sh
+++
@@ -1,7 +0,0 @@
-
-clear
-arg1=$1
-arg2=${2:-'e'}
-
-bash ./lib/repeat.sh 100 "bash ./lib/guess-wifi-password.sh -$arg1$arg1" | grep $arg2 | sort -u | pv -l -s 16 -p -e -t -i 0.1 -c -N "Mencari hasil yang terbaik..." | pick | termux-clipboard-set;
-termux-toast "Password berhasil disalin!"
